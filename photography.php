396422c16980a4e719b2b91d2d42e1129095f299
Jazsmin Gilliam <jazzlynetgeeter@gmail.com>
2024-10-15 21:27:14 -0500

    Created a photo gallery page

---
+++ photography.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang = "en"></html>
+    <link rel = "stylesheet" href="stylesheet.css">
+
+    <div>
+        <head>
+        <meta charset="UTF-8" />
+        <title> Welcome to the HomePage </title>
+        <div>
+            <nav>
+                <ul class= "navigation_bar">
+                    <li> <a href = "index.html"> Home &nbsp;</a> </li>
+                </ul>
+            </nav>
+        </div>
+            
+    </head>
+    </div>
+    
+    <body>
+        <div>
+        <h1><strong>Photo Gallery</strong></h1>
+        </div>
+
+    </body>
+
+</html>
